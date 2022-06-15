a42b766964f67120ee274bc9ff1c83d12f78cb73
wlmchrstn <wlmchrstn@gmail.com>
2022-06-15 20:37:01 +0700

    [will] add title component

--- src/pages/landing/landing.js
+++ src/pages/landing/landing.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Title from '../../components/title/title';
 import styles from './landing.module.scss';
 
 const LandingPage = () => {
@@ -7,6 +8,13 @@ const LandingPage = () => {
             <h1>Landing Page</h1>
             <h2>test pr</h2>
             <h3>test eslint</h3>
+            <Title
+                tagElement={'h1'}
+                variant={'heading-1-light'}
+                color={'white'}
+            >
+                Test title
+            </Title>
         </div>
     );
 };
