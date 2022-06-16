f73d9a25f96079fc24c64b9e67f73d639a7a7349
wlmchrstn <wlmchrstn@gmail.com>
2022-06-16 19:55:44 +0700

    [will] remove test

--- src/pages/landing/landing.js
+++ src/pages/landing/landing.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import Title from '../../components/title/title';
 import styles from './landing.module.scss';
 
 const LandingPage = () => {
@@ -8,13 +7,6 @@ const LandingPage = () => {
             <h1>Landing Page</h1>
             <h2>test pr</h2>
             <h3>test eslint</h3>
-            <Title
-                tagElement={'h1'}
-                variant={'heading-1-light'}
-                color={'white'}
-            >
-                Test title
-            </Title>
         </div>
     );
 };
