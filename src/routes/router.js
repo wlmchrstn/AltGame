975da0313c42b925fa36490147f9babd10cb8615
SonefA <mushonnefa@gmail.com>
2022-06-17 20:07:55 +0700

    [snf] add login page

--- src/routes/router.js
+++ src/routes/router.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
+import Login from '../pages/login/login';
 import LandingPage from '../pages/landing/landing';
 import Custom404 from '../pages/custom404/custom404';
 
 const Router = () => {
     return (
         <Routes>
+            <Route path={'/Login'} element={<Login />} />
             <Route path={'/'} element={<LandingPage />} />
             <Route path={'*'} element={<Custom404 />} />
         </Routes>
