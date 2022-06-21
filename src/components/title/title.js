d3c5c3cc9d73b1ec011a60e49197dfc3165caead
wlmchrstn <wlmchrstn@gmail.com>
2022-06-21 19:36:54 +0700

    [will] update paragraph and title

--- src/components/title/title.js
+++ src/components/title/title.js
@@ -3,12 +3,13 @@ import PropTypes from 'prop-types';
 import classNames from 'classnames';
 import styles from './title.module.scss';
 
-const Title = ({ children, tagElement, variant, color, ...props }) => {
+const Title = ({ children, tagElement, variant, color, weight, ...props }) => {
     const baseProps = {
         className: classNames(
             styles.root,
             styles[variant],
             styles[color],
+            styles[weight],
             props.className
         ),
     };
@@ -34,6 +35,7 @@ Title.propTypes = {
     tagElement: PropTypes.string,
     variant: PropTypes.string,
     color: PropTypes.string,
+    weight: PropTypes.string,
     className: PropTypes.string,
 };
 
@@ -42,6 +44,8 @@ Title.defaultProps = {
     tagElement: 'h1',
     variant: 'heading-1',
     color: 'black',
+    weight: null,
+    className: null,
 };
 
 export default Title;
