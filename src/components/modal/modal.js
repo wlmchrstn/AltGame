07b9b47a01d547d8073d8d7bea15146650a93868
wlmchrstn <wlmchrstn@gmail.com>
2022-06-21 10:54:44 +0700

    [will] create modal component

---
+++ src/components/modal/modal.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { createPortal } from 'react-dom';
+import classNames from 'classnames';
+import styles from './modal.module.scss';
+import x from '../../assets/icons/fi_x.svg';
+
+const Modal = ({ children, open, onClose, className }) => {
+    if (!open) return null;
+    return createPortal(
+        <>
+            <div className={styles.overlay} onClick={onClose}></div>
+            <div className={classNames(styles.content, className)}>
+                <img
+                    className={styles.image}
+                    src={x}
+                    alt={'fi_x'}
+                    onClick={onClose}
+                />
+                {children}
+            </div>
+        </>,
+        document.getElementById('portal')
+    );
+};
+
+Modal.propTypes = {
+    children: PropTypes.node.isRequired,
+    onClose: PropTypes.func,
+    open: PropTypes.bool,
+    className: PropTypes.any,
+};
+
+Modal.defaultProps = {
+    children: <input />,
+    onClose: null,
+    open: false,
+    className: null,
+};
+
+export default Modal;
--- src/pages/product/product.js
+++ src/pages/product/product.js
@@ -1,15 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
 // import { useParams } from 'react-router-dom';
+import { useForm } from 'react-hook-form';
 import styles from './product.module.scss';
-import imgPlaceholder from '../../assets/images/login-bg.png';
+
+// Components
 import Button from '../../components/button/button';
+import Modal from '../../components/modal/modal';
+import Input from '../../components/input/input';
+
+// Assets
+import imgPlaceholder from '../../assets/images/login-bg.png';
 
 const ProductPage = () => {
     // const { id } = useParams();
+    const {
+        register,
+        formState: { errors },
+        handleSubmit,
+    } = useForm();
+    const [isOpen, setIsOpen] = useState(false);
+
+    const handleClick = () => {
+        setIsOpen(true);
+    };
+
+    const handleTawar = data => {
+        console.log(data);
+    };
+
     return (
         <section className={styles.root}>
             <div className={styles.left}>
-                <div className={styles.carousel}></div>
+                <div className={styles.carousel}>
+                    <img src={imgPlaceholder} alt={'placeholder'} />
+                </div>
                 <div className={styles.desc}>
                     <p className={styles['desc-header']}>Deskripsi</p>
                     <p className={styles['desc-body']}>
@@ -41,9 +65,54 @@ const ProductPage = () => {
                     <p>Jam Tangan Casio</p>
                     <p>Aksesoris</p>
                     <p>Rp 250.000</p>
-                    <Button type={'button'} variant={'primary'}>
+                    <Button
+                        type={'button'}
+                        variant={'primary'}
+                        onClick={handleClick}
+                    >
                         Saya tertarik dan ingin nego
                     </Button>
+                    <Modal
+                        open={isOpen}
+                        onClose={() => setIsOpen(false)}
+                        className={styles.modal}
+                    >
+                        <p>Masukkan Harga Tawarmu</p>
+                        <p>
+                            Harga tawaranmu akan diketahui penual, jika penjual
+                            cocok kamu akan segera dihubungi penjual.
+                        </p>
+                        <div className={styles['product-summary']}>
+                            <img
+                                className={styles['product-image']}
+                                src={imgPlaceholder}
+                                alt={'seller-image'}
+                            />
+                            <div className={styles['product-detail']}>
+                                <p>Jam Tangan Casio</p>
+                                <p>Rp 250.000</p>
+                            </div>
+                        </div>
+                        <p>Harga Tawar</p>
+                        <form onSubmit={handleSubmit(handleTawar)}>
+                            <Input>
+                                <input
+                                    type={'number'}
+                                    placeholder={'Rp 0,00'}
+                                    {...register('harga', { required: true })}
+                                />
+                                {errors.harga &&
+                                    errors.harga.type === 'required' && (
+                                        <p className={styles.error}>
+                                            *Required field*
+                                        </p>
+                                    )}
+                            </Input>
+                            <Button type={'submit'} variant={'primary'}>
+                                Kirim
+                            </Button>
+                        </form>
+                    </Modal>
                 </div>
                 <div className={styles.seller}>
                     <img
