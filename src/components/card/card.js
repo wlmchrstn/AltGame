b99c767abc25c1d462dc2225c2487a39f89c48aa
wlmchrstn <wlmchrstn@gmail.com>
2022-06-21 23:29:27 +0700

    [will] fix page/landing

--- src/components/card/card.js
+++ src/components/card/card.js
@@ -2,20 +2,29 @@ import React from 'react';
 import styles from './card.module.scss';
 import PropTypes from 'prop-types';
 
-import Title from '../title/title';
+import Paragraph from '../paragraph/paragraph';
 import imgcard from '../../assets/images/card-image.png';
 
 const Card = ({ title }) => {
-    const handleTitle = title ? title : '';
     return (
         <div className={styles.root}>
             <div className={styles.image}>
                 <img src={imgcard} alt={'console'} />
             </div>
             <div className={styles.wrapper}>
-                <Title tagElement={'h2'}>{handleTitle}</Title>
-                <p>{'Aksesoris'}</p>
-                <Title tagElement={'h2'}>{'Rp 250.000'}</Title>
+                <Paragraph className={styles.title} variant={'body-1'}>
+                    {title}
+                </Paragraph>
+                <Paragraph
+                    className={styles.category}
+                    variant={'body-3'}
+                    color={'grey'}
+                >
+                    {'Aksesoris'}
+                </Paragraph>
+                <Paragraph className={styles.price} variant={'body-1'}>
+                    {'Rp 250.000'}
+                </Paragraph>
             </div>
         </div>
     );
@@ -26,35 +35,7 @@ Card.propTypes = {
 };
 
 Card.defaultProps = {
-    children: '',
-    variant: 'primary',
-    color: '',
-    type: 'button',
-    withIcon: '',
+    title: 'Jam Tangan Casio',
 };
 
 export default Card;
-
-// import React from 'react';
-// import styles from './card.module.scss';
-// import { useNavigate } from 'react-router-dom';
-
-// import Title from '../title/title';
-
-// const card = ({ name, category, price, image }) => {
-//     const navigate = useNavigate();
-//     return (
-//         <div className={styles.root}>
-//             <div className={styles.image}>
-//                 <img src={image} alt={'console'} />
-//             </div>
-//             <div className={styles.wrapper}>
-//                 <Title tagElement={'h2'}>{'${name}'}</Title>
-//                 <p>{'${category}'}</p>
-//                 <Title tagElement={'h2'}>{'${formatRupiah(price)}'}</Title>
-//             </div>
-//         </div>
-//     );
-// };
-
-// export default card;
