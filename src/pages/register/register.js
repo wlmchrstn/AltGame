5a8a654f7c64decab91e34da6b0533fa2df27423
wlmchrstn <wlmchrstn@gmail.com>
2022-06-23 13:18:22 +0700

    [will] update user-flow

--- src/pages/register/register.js
+++ src/pages/register/register.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { useForm } from 'react-hook-form';
+import { Link } from 'react-router-dom';
 import styles from './register.module.scss';
 
 import Title from '../../components/title/title';
 import Input from '../../components/input/input';
 import Button from '../../components/button/button';
-import { Link } from 'react-router-dom';
+import Paragraph from '../../components/paragraph/paragraph';
 
 const Register = () => {
     const {
@@ -23,8 +24,9 @@ const Register = () => {
             <div className={styles.wrapper}>
                 <Title
                     tagElement={'h1'}
-                    variant={'heading-1-bold'}
+                    variant={'heading-1'}
                     color={'black'}
+                    weight={'bold'}
                 >
                     {'Daftar'}
                 </Title>
@@ -32,8 +34,10 @@ const Register = () => {
                     onSubmit={handleSubmit(handleRegister)}
                     className={styles.form}
                 >
-                    <p>{'Nama'}</p>
-                    <Input>
+                    <Paragraph variant={'body-2'} className={styles.label}>
+                        {'Nama'}
+                    </Paragraph>
+                    <Input className={styles.input}>
                         <input
                             type={'text'}
                             placeholder={'Nama lengkap'}
@@ -43,8 +47,10 @@ const Register = () => {
                     {errors.nama && errors.nama.type === 'required' && (
                         <p className={styles.error}>*Required field*</p>
                     )}
-                    <p>{'Email'}</p>
-                    <Input>
+                    <Paragraph variant={'body-2'} className={styles.label}>
+                        {'Email'}
+                    </Paragraph>
+                    <Input className={styles.input}>
                         <input
                             type={'email'}
                             placeholder={'Contoh: johndee@gmail.com'}
@@ -54,8 +60,10 @@ const Register = () => {
                     {errors.email && errors.email.type === 'required' && (
                         <p className={styles.error}>*Required field*</p>
                     )}
-                    <p>{'Password'}</p>
-                    <Input>
+                    <Paragraph variant={'body-2'} className={styles.label}>
+                        {'Password'}
+                    </Paragraph>
+                    <Input className={styles.input}>
                         <input
                             type={'password'}
                             placeholder={'Masukkan password'}
@@ -70,7 +78,9 @@ const Register = () => {
                     </Button>
                 </form>
                 <div className={styles.login}>
-                    <p>{'Sudah punya akun?'}</p>
+                    <Paragraph variant={'body-2'}>
+                        {'Sudah punya akun?'}
+                    </Paragraph>
                     <Link to={'/login'}>{'Masuk di sini'}</Link>
                 </div>
             </div>
