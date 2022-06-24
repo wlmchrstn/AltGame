255f223d7c2310181504d05fbacf13cf26bcc9cf
SonefA <mushonnefa@gmail.com>
2022-06-24 20:45:17 +0700

    [snf] add seller landing

--- src/routes/router.js
+++ src/routes/router.js
@@ -6,6 +6,7 @@ import LandingPage from '../pages/landing/landing';
 import ProductPage from '../pages/product/product';
 import Custom404 from '../pages/custom404/custom404';
 import Register from '../pages/register/register';
+import SellerPage from '../pages/seller/seller';
 
 import Navbar from '../modules/navbar/navbar';
 
@@ -24,6 +25,7 @@ const Router = () => {
             <Route element={<WithNav />}>
                 <Route path={'/'} element={<LandingPage />} />
                 <Route path={'/product/:id'} element={<ProductPage />} />
+                <Route path={'/seller'} element={<SellerPage />} />
             </Route>
             <Route path={'/login'} element={<LoginPage />} />
             <Route path={'/register'} element={<Register />} />
