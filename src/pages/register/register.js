bdca07562e06d04509d87dc3a587a77cee052df2
wlmchrstn <wlmchrstn@gmail.com>
2022-07-04 20:09:11 +0700

    [will] modify register and landing

--- src/pages/register/register.js
+++ src/pages/register/register.js
@@ -21,98 +21,102 @@ const Register = () => {
 
     return (
         <section className={styles.root}>
-            <div className={styles.wrapper}>
-                <Title
-                    tagElement={'h1'}
-                    variant={'heading-1'}
-                    color={'black'}
-                    weight={'bold'}
-                >
-                    {'Daftar'}
-                </Title>
-                <form
-                    onSubmit={handleSubmit(handleRegister)}
-                    className={styles.form}
-                >
-                    <Paragraph variant={'body-2'} className={styles.label}>
-                        {'Nama'}
-                    </Paragraph>
-                    <Input className={styles.input}>
-                        <input
-                            type={'text'}
-                            name={'name'}
-                            placeholder={'Nama lengkap'}
-                            {...register('nama', { required: true })}
-                        />
-                    </Input>
-                    {errors.nama && errors.nama.type === 'required' && (
-                        <p className={styles.error}>*Required field*</p>
-                    )}
-                    <Paragraph variant={'body-2'} className={styles.label}>
-                        {'Username'}
-                    </Paragraph>
-                    <Input className={styles.input}>
-                        <input
-                            type={'text'}
-                            name={'username'}
-                            placeholder={'Masukkan Username'}
-                            {...register('username', { required: true })}
-                        />
-                    </Input>
-                    {errors.username && errors.username.type === 'required' && (
-                        <p className={styles.error}>*Required field*</p>
-                    )}
-                    <Paragraph variant={'body-2'} className={styles.label}>
-                        {'Email'}
-                    </Paragraph>
-                    <Input className={styles.input}>
-                        <input
-                            type={'email'}
-                            name={'email'}
-                            placeholder={'Contoh: johndee@gmail.com'}
-                            {...register('email', { required: true })}
-                        />
-                    </Input>
-                    {errors.email && errors.email.type === 'required' && (
-                        <p className={styles.error}>*Required field*</p>
-                    )}
-                    <Paragraph variant={'body-2'} className={styles.label}>
-                        {'Password'}
-                    </Paragraph>
-                    <Input className={styles.input}>
-                        <input
-                            type={'password'}
-                            name={'password'}
-                            placeholder={'Masukkan password'}
-                            {...register('password', { required: true })}
-                        />
-                    </Input>
-                    {errors.password && errors.password.type === 'required' && (
-                        <p className={styles.error}>*Required field*</p>
-                    )}
-                    <Paragraph variant={'body-2'} className={styles.label}>
-                        {'Phone'}
-                    </Paragraph>
-                    <Input className={styles.input}>
-                        <input
-                            type={'number'}
-                            name={'phone'}
-                            placeholder={'Contoh: 087898685848'}
-                            {...register('phone', { required: true })}
-                        />
-                    </Input>
-                    {errors.phone && errors.phone.type === 'required' && (
-                        <p className={styles.error}>*Required field*</p>
-                    )}
-                    <Button type={'submit'} variant={'primary'}>
+            <div className={styles.container}>
+                <div className={styles.wrapper}>
+                    <Title
+                        tagElement={'h1'}
+                        variant={'heading-1'}
+                        color={'black'}
+                        weight={'bold'}
+                    >
                         {'Daftar'}
-                    </Button>
-                </form>
-                <div className={styles.login}>
-                    <Paragraph variant={'body-2'}>
-                        {'Sudah punya akun?'}
-                    </Paragraph>
-                    <Link to={'/login'}>{'Masuk di sini'}</Link>
+                    </Title>
+                    <form
+                        onSubmit={handleSubmit(handleRegister)}
+                        className={styles.form}
+                    >
+                        <Paragraph variant={'body-2'} className={styles.label}>
+                            {'Nama'}
+                        </Paragraph>
+                        <Input className={styles.input}>
+                            <input
+                                type={'text'}
+                                name={'name'}
+                                placeholder={'Nama lengkap'}
+                                {...register('nama', { required: true })}
+                            />
+                        </Input>
+                        {errors.nama && errors.nama.type === 'required' && (
+                            <p className={styles.error}>*Required field*</p>
+                        )}
+                        <Paragraph variant={'body-2'} className={styles.label}>
+                            {'Username'}
+                        </Paragraph>
+                        <Input className={styles.input}>
+                            <input
+                                type={'text'}
+                                name={'username'}
+                                placeholder={'Masukkan Username'}
+                                {...register('username', { required: true })}
+                            />
+                        </Input>
+                        {errors.username &&
+                            errors.username.type === 'required' && (
+                                <p className={styles.error}>*Required field*</p>
+                            )}
+                        <Paragraph variant={'body-2'} className={styles.label}>
+                            {'Email'}
+                        </Paragraph>
+                        <Input className={styles.input}>
+                            <input
+                                type={'email'}
+                                name={'email'}
+                                placeholder={'Contoh: johndee@gmail.com'}
+                                {...register('email', { required: true })}
+                            />
+                        </Input>
+                        {errors.email && errors.email.type === 'required' && (
+                            <p className={styles.error}>*Required field*</p>
+                        )}
+                        <Paragraph variant={'body-2'} className={styles.label}>
+                            {'Password'}
+                        </Paragraph>
+                        <Input className={styles.input}>
+                            <input
+                                type={'password'}
+                                name={'password'}
+                                placeholder={'Masukkan password'}
+                                {...register('password', { required: true })}
+                            />
+                        </Input>
+                        {errors.password &&
+                            errors.password.type === 'required' && (
+                                <p className={styles.error}>*Required field*</p>
+                            )}
+                        <Paragraph variant={'body-2'} className={styles.label}>
+                            {'Phone'}
+                        </Paragraph>
+                        <Input className={styles.input}>
+                            <input
+                                type={'number'}
+                                name={'phone'}
+                                placeholder={'Contoh: 087898685848'}
+                                {...register('phone', { required: true })}
+                            />
+                        </Input>
+                        {errors.phone && errors.phone.type === 'required' && (
+                            <p className={styles.error}>*Required field*</p>
+                        )}
+                        <Button type={'submit'} variant={'primary'}>
+                            {'Daftar'}
+                        </Button>
+                    </form>
+                    <div className={styles.login}>
+                        <Paragraph variant={'body-2'}>
+                            {'Sudah punya akun?'}
+                        </Paragraph>
+                        <Link to={'/login'}>{'Masuk di sini'}</Link>
+                    </div>
                 </div>
             </div>
         </section>
